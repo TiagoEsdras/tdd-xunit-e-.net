f97d29b16d73ebf89de10d1b8535c31f2545eccb
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-03-14 19:04:52 -0300

    Refatorando publico alvo

--- CursoOnline/test/CursoOnline.Domain.Tests/Cursos/CursoTest.cs
+++ CursoOnline/test/CursoOnline.Domain.Tests/Cursos/CursoTest.cs
@@ -7,7 +7,8 @@ namespace CursoOnline.Domain.Tests.Cursos
     public class CursoTest
     {
         /// <summary>
-        /// Deve criar um curso com nome, carga horária, público alvo, e valor do curso
+        /// Deve criar um curso com nome, carga horária, público alvo, e valor do curso;
+        /// As opções para PublicoAlvo devem ser: "Estudante", "Universitário", "Empregado" e "Empreendedor";
         /// </summary>
         [Fact]
         public void DeveCriarUmCurso()
@@ -16,7 +17,7 @@ namespace CursoOnline.Domain.Tests.Cursos
             {
                 Nome = "Curso 01",
                 CargaHoraria = 500,
-                PublicoAlvo = "Estudantes",
+                PublicoAlvo = PublicoAlvoEnum.Estudante,
                 Valor = 199.99m
             };
 
@@ -28,7 +29,7 @@ namespace CursoOnline.Domain.Tests.Cursos
 
     public class Curso
     {
-        public Curso(string nome, int cargaHoraria, string publicoAlvo, decimal valor)
+        public Curso(string nome, int cargaHoraria, PublicoAlvoEnum publicoAlvo, decimal valor)
         {
             Nome = nome;
             CargaHoraria = cargaHoraria;
@@ -38,7 +39,15 @@ namespace CursoOnline.Domain.Tests.Cursos
 
         public string Nome { get; private set; }
         public int CargaHoraria { get; private set; }
-        public string PublicoAlvo { get; private set; }
+        public PublicoAlvoEnum PublicoAlvo { get; private set; }
         public decimal Valor { get; private set; }
+    }
+
+    public enum PublicoAlvoEnum
+    {
+        Estudante,
+        Universitario,
+        Empregado,
+        Empreendedor
     }
 }
