e990c27c94ff020c47f954a82bbd86af1db4b4d4
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-03-14 18:49:18 -0300

    Criando teste DeveCriarUmCurso - falha

---
+++ CursoOnline/test/CursoOnline.Domain.Tests/Cursos/CursoTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+
+namespace CursoOnline.Domain.Tests.Cursos
+{
+    public class CursoTest
+    {
+        /// <summary>
+        /// Deve criar um curso com nome, carga horária, público alvo, e valor do curso
+        /// </summary>
+        [Fact]
+        public void DeveCriarUmCurso()
+        {
+            string nome = "Curso 01";
+            int cargaHoraria = 500;
+            string publicoAlvo = "Estudantes";
+            decimal valor = 199.99m;
+
+            var curso = new Curso(nome, cargaHoraria, publicoAlvo, valor);
+
+            Assert.Equal(nome, curso.Nome);
+            Assert.Equal(cargaHoraria, curso.CargaHoraria);
+            Assert.Equal(publicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(valor, curso.Valor);
+        }
+    }
+
+    public class Curso
+    {
+        public Curso(string nome, int cargaHoraria, string publicoAlvo, decimal valor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public string PublicoAlvo { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
