36cea4e514a30ea5b4596cd9d8da70fd27f05547
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-03-14 19:41:39 -0300

    Criação de classe statica AssertExtension

---
+++ CursoOnline/test/CursoOnline.Domain.Tests/Utils/AssertExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace CursoOnline.Domain.Tests
+{
+    public static class AssertExtension
+    {
+        public static void ComMensagem(this ArgumentException ex, string mensagem)
+        {
+            if (ex.Message == mensagem)
+                Assert.True(true);
+            else
+                Assert.True(false, $"Erro esperado: '{mensagem}'; Erro lançado: '{ex.Message}'");
+        }
+    }
+}
