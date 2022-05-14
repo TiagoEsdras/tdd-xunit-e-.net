57041267cfaa53759409937a0ef14b5a6d36d5a8
Tiago Rodrigues <tiago.rodrigues@pottencial.com.br>
2022-05-14 15:06:47 -0300

    Adicionado propriedade Descricao a classe Curso, e testando se essa propriedade é válida na instanciação do objeto Curso

--- CursoOnline/test/CursoOnline.Domain.Tests/Cursos/CursoTest.cs
+++ CursoOnline/test/CursoOnline.Domain.Tests/Cursos/CursoTest.cs
@@ -7,6 +7,7 @@ namespace CursoOnline.Domain.Tests.Cursos
     public class CursoTest
     {
         private readonly string _nome;
+        private readonly string _descricao;
         private readonly int _cargaHoraria;
         private readonly PublicoAlvoEnum _publicoAlvo;
         private readonly decimal _valor;
@@ -14,6 +15,7 @@ namespace CursoOnline.Domain.Tests.Cursos
         public CursoTest()
         {
             _nome = "Curso 01";
+            _descricao = "Descricao do curso";
             _cargaHoraria = 500;
             _publicoAlvo = PublicoAlvoEnum.Estudante;
             _valor = 199.99m;
@@ -29,12 +31,13 @@ namespace CursoOnline.Domain.Tests.Cursos
             var cursoEsperado = new
             {
                 Nome = "Curso 01",
+                Descricao = "Descricao do curso",
                 CargaHoraria = 500,
                 PublicoAlvo = PublicoAlvoEnum.Estudante,
                 Valor = 199.99m
             };
 
-            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
+            var curso = new Curso(cursoEsperado.Nome, cursoEsperado.Descricao, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor);
 
             cursoEsperado.ToExpectedObject().ShouldMatch(curso);
         }
@@ -46,10 +49,23 @@ namespace CursoOnline.Domain.Tests.Cursos
         [InlineData("")]
         [InlineData(null)]
         public void NaoDeveCursoTerNomeInvalido(string nomeInvalido)
-        {           
+        {
             Assert.Throws<ArgumentException>(() =>
-                new Curso(nomeInvalido, _cargaHoraria, _publicoAlvo, _valor)
+                new Curso(nomeInvalido, _descricao, _cargaHoraria, _publicoAlvo, _valor)
             ).ComMensagem("Nome não pode ser nulo ou uma string vazia");
+        }
+
+        /// <summary>
+        /// Descricao não pode ser invalida
+        /// </summary>
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NaoDeveCursoTerDescricaoInvalida(string descricaoInvalida)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Curso(_nome, descricaoInvalida, _cargaHoraria, _publicoAlvo, _valor)
+            ).ComMensagem("Descrição não pode ser nula ou uma string vazia");
         }
 
         /// <summary>
@@ -60,11 +76,9 @@ namespace CursoOnline.Domain.Tests.Cursos
         [InlineData(-50)]
         public void NaoDeveCursoTerCargaHorariaInvalida(int cargaHorariaInvalida)
         {
-            
-
-           Assert.Throws<ArgumentException>(() =>
-                new Curso(_nome, cargaHorariaInvalida, _publicoAlvo, _valor)
-            ).ComMensagem("Carga horária não pode ser menor ou igual a zero");
+            Assert.Throws<ArgumentException>(() =>
+                 new Curso(_nome, _descricao, cargaHorariaInvalida, _publicoAlvo, _valor)
+             ).ComMensagem("Carga horária não pode ser menor ou igual a zero");
         }
 
         /// <summary>
@@ -75,26 +89,21 @@ namespace CursoOnline.Domain.Tests.Cursos
         [InlineData(-50.88)]
         public void NaoDeveCursoTerValorInvalido(decimal valorInvalido)
         {
-            var cursoEsperado = new
-            {
-                Nome = "Curso 01",
-                CargaHoraria = 500,
-                PublicoAlvo = PublicoAlvoEnum.Estudante,
-                Valor = 199.99m
-            };
-
             Assert.Throws<ArgumentException>(() =>
-                new Curso(_nome, _cargaHoraria, cursoEsperado.PublicoAlvo, valorInvalido)
+                new Curso(_nome, _descricao, _cargaHoraria, _publicoAlvo, valorInvalido)
             ).ComMensagem("Valor do curso não pode ser menor ou igual a zero");
         }
     }
 
     public class Curso
     {
-        public Curso(string nome, int cargaHoraria, PublicoAlvoEnum publicoAlvo, decimal valor)
+        public Curso(string nome, string descricao, int cargaHoraria, PublicoAlvoEnum publicoAlvo, decimal valor)
         {
             if (string.IsNullOrEmpty(nome))
                 throw new ArgumentException("Nome não pode ser nulo ou uma string vazia");
+
+            if (string.IsNullOrEmpty(descricao))
+                throw new ArgumentException("Descrição não pode ser nula ou uma string vazia");
 
             if (cargaHoraria <= 0)
                 throw new ArgumentException("Carga horária não pode ser menor ou igual a zero");
@@ -103,12 +112,14 @@ namespace CursoOnline.Domain.Tests.Cursos
                 throw new ArgumentException("Valor do curso não pode ser menor ou igual a zero");
 
             Nome = nome;
+            Descricao = descricao;
             CargaHoraria = cargaHoraria;
             PublicoAlvo = publicoAlvo;
             Valor = valor;
         }
 
         public string Nome { get; private set; }
+        public string Descricao { get; private set; }
         public int CargaHoraria { get; private set; }
         public PublicoAlvoEnum PublicoAlvo { get; private set; }
         public decimal Valor { get; private set; }
