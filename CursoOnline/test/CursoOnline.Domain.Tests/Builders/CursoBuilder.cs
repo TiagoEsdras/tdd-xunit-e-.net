9e8b22e07aa285b097a5ab3aa6997c8a9506afbb
Tiago Rodrigues <tiago.rodrigues@pottencial.com.br>
2022-05-14 15:11:55 -0300

    Criando classe CursoBuilder com metodos para facilitar a instanciação de um objeto da classe Curso

---
+++ CursoOnline/test/CursoOnline.Domain.Tests/Builders/CursoBuilder.cs
@@ -0,0 +1,53 @@
+﻿using CursoOnline.Domain.Tests.Cursos;
+
+namespace CursoOnline.Domain.Tests.Builders
+{
+    public class CursoBuilder
+    {
+        private string _nome = "Nome do curso";
+        private string _descricao = "Descrição do curso";
+        private int _cargaHoraria = 80;
+        private PublicoAlvoEnum _publicoAlvo = PublicoAlvoEnum.Estudante;
+        private decimal _valor = 950;
+
+        public static CursoBuilder Novo()
+        {
+            return new CursoBuilder();
+        }
+
+        public CursoBuilder ComNome(string nome)
+        {
+            _nome = nome;
+            return this;
+        }
+
+        public CursoBuilder ComDescricao(string descricao)
+        {
+            _descricao = descricao;
+            return this;
+        }
+
+        public CursoBuilder ComCargaHoraria(int cargaHoraria)
+        {
+            _cargaHoraria = cargaHoraria;
+            return this;
+        }
+
+        public CursoBuilder ComValor(decimal valor)
+        {
+            _valor = valor;
+            return this;
+        }
+
+        public CursoBuilder ComPublicoAlvo(PublicoAlvoEnum publicoAlvo)
+        {
+            _publicoAlvo = publicoAlvo;
+            return this;
+        }
+
+        public Curso Build()
+        {
+            return new Curso(_nome, _descricao, _cargaHoraria, _publicoAlvo, _valor);
+        }
+    }
+}
