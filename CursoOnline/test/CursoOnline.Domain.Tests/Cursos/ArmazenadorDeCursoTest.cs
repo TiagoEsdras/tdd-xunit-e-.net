66a3a0181cd11e679e95bef729026aff7247c848
Tiago Rodrigues <tiago.rodrigues@pottencial.com.br>
2022-05-15 11:18:24 -0300

    Criando ArmazenadorDeCursoTest e verificando que o metodo Adiconar serja chamado ao tentar adicionar curso

---
+++ CursoOnline/test/CursoOnline.Domain.Tests/Cursos/ArmazenadorDeCursoTest.cs
@@ -0,0 +1,60 @@
+﻿using CursoOnline.Domain.Cursos;
+using CursoOnline.Domain.Enums;
+using Moq;
+using System;
+using Xunit;
+
+namespace CursoOnline.Domain.Tests.Cursos
+{
+    public class ArmazenadorDeCursoTest
+    {
+        [Fact]
+        public void DeveAdicinarCurso()
+        {
+            var cursoDto = new CursoDto
+            {
+                Nome = "Curso A",
+                Descricao = "Descricao curso A",
+                CargaHoraria = 80,
+                PublicoAlvo = 1,
+                Valor = 599.99m
+            };
+            var cursoRepositorioMock = new Mock<ICursoRepositorio>();
+            var armazenadorDeCurso = new ArmazenadorDeCurso(cursoRepositorioMock.Object);
+
+            armazenadorDeCurso.Armazenar(cursoDto);
+
+            cursoRepositorioMock.Verify(r => r.Adicionar(It.IsAny<Curso>()));
+        }
+    }
+
+    public interface ICursoRepositorio
+    {
+        void Adicionar(Curso curso);
+    }
+
+    public class ArmazenadorDeCurso
+    {
+        private ICursoRepositorio _cursoRepositorio;
+
+        public ArmazenadorDeCurso(ICursoRepositorio cursoRepositorio)
+        {
+            _cursoRepositorio = cursoRepositorio;
+        }
+
+        public void Armazenar(CursoDto cursoDto)
+        {
+            var curso = new Curso(cursoDto.Nome, cursoDto.Descricao, cursoDto.CargaHoraria, PublicoAlvoEnum.Estudante, cursoDto.Valor);
+            _cursoRepositorio.Adicionar(curso);
+        }
+    }
+
+    public class CursoDto
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public int CargaHoraria { get; set; }
+        public int PublicoAlvo { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
