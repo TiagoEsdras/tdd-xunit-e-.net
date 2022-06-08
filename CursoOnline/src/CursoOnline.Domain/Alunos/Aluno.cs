491c0dec1c77c7b7fd248100fb56654bdc189929
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-06-08 00:16:00 -0300

    Criando classe aluno

---
+++ CursoOnline/src/CursoOnline.Domain/Alunos/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using CursoOnline.Domain.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoOnline.Domain.Alunos
+{
+    public class Aluno
+    {
+        [Key]
+        public Guid Id { get; private set; }
+
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
+        public PublicoAlvoEnum PublicoAlvo { get; private set; }
+
+        public Aluno(string nome, string cpf, string email, PublicoAlvoEnum publicoAlvo)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
+            PublicoAlvo = publicoAlvo;
+        }
+    }
+}
