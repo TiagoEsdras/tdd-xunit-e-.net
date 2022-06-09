8d10886e19cdafffbefe3600d0ef4940ba8d266d
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-06-08 21:49:12 -0300

    Criando aluno controller

---
+++ CursoOnline/src/CursoOnline.API/Controllers/AlunosController.cs
@@ -0,0 +1,50 @@
+﻿using CursoOnline.Application.Contratos;
+using CursoOnline.Application.Dtos.Alunos;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CursoOnline.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunosController : ControllerBase
+    {
+        private readonly IAlunoService _alunoService;
+
+        public AlunosController(IAlunoService alunoService)
+        {
+            _alunoService = alunoService;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Post(CreateAlunoDto createAlunoDto)
+        {
+            try
+            {
+                await _alunoService.Adicionar(createAlunoDto);
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(new { message = ex.Message }) { StatusCode = 500 };
+            }
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(200, Type = typeof(AlunoDto))]
+        public async Task<IActionResult> Update(Guid id, UpdateAlunoDto updateAlunoDto)
+        {
+            try
+            {
+                await _alunoService.Atualizar(updateAlunoDto);
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(new { message = ex.Message }) { StatusCode = 500 };
+            }
+        }
+    }
+}
---
+++ CursoOnline/src/CursoOnline.Application/Dtos/Alunos/AlunoDto.cs
@@ -0,0 +1,27 @@
+﻿using CursoOnline.Domain.Alunos;
+using System;
+
+namespace CursoOnline.Application.Dtos.Alunos
+{
+    public class AlunoDto
+    {
+        public AlunoDto()
+        {
+        }
+
+        public AlunoDto(Aluno aluno)
+        {
+            Id = aluno.Id;
+            Nome = aluno.Nome;
+            CPF = aluno.CPF;
+            Email = aluno.Email;
+            PublicoAlvo = aluno.PublicoAlvo.ToString();
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string PublicoAlvo { get; set; }
+    }
+}
