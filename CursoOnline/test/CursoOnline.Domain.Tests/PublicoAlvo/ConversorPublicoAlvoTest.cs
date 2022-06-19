bc1636964deb97fe61ed86e83b71f8d5e0ccd6be
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2022-06-19 15:47:09 -0300

    criando testes para conversos de publico alvo

---
+++ CursoOnline/test/CursoOnline.Domain.Tests/PublicoAlvo/ConversorPublicoAlvoTest.cs
@@ -0,0 +1,40 @@
+﻿using CursoOnline.Application;
+using CursoOnline.Domain.Constants;
+using CursoOnline.Domain.Enums;
+using System;
+using Xunit;
+
+namespace CursoOnline.Domain.Tests.PublicoAlvo
+{
+    public class ConversorPublicoAlvoTest
+    {
+        private readonly ConversorPublicoAlvo _conversorPublicoAlvo;
+
+        public ConversorPublicoAlvoTest()
+        {
+            _conversorPublicoAlvo = new ConversorPublicoAlvo();
+        }
+
+        [Theory]
+        [InlineData(PublicoAlvoEnum.Estudante, "Estudante")]
+        [InlineData(PublicoAlvoEnum.Universitario, "Universitario")]
+        [InlineData(PublicoAlvoEnum.Empregado, "Empregado")]
+        [InlineData(PublicoAlvoEnum.Empreendedor, "Empreendedor")]
+        public void DeveConverterPublicoAlvo(PublicoAlvoEnum publicoAlvo, string publicoAlvoString)
+        {
+            var publicoAlvoConvertido = _conversorPublicoAlvo.Converter(publicoAlvoString);
+
+            Assert.Equal(publicoAlvoConvertido, publicoAlvo);
+        }
+
+        [Fact]
+        public void DeveLancarExcecaoAoTentarConverterPublicoAlvoInvalido()
+        {
+            var publicoAlvoInvalido = "Medico";
+
+            var error = Assert.Throws<ArgumentException>(() => _conversorPublicoAlvo.Converter(publicoAlvoInvalido));
+
+            error.ComMensagem(ErroMessage.PUBLICO_ALVO_INVALIDO);
+        }
+    }
+}
